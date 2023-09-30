0f929d2272130bca31ef67df579771ca73407564
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-09-30 14:03:32 +0100

    Created custom pagination with animation and navigation for projects slider

--- js/index.js
+++ js/index.js
@@ -10,6 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	const themeBtn = document.querySelector('.theme-btn');
 	const navBtn = document.querySelector('.navigation__btn');
 	const footerYear = document.getElementById('footerYear');
+	const projectsSlider = document.querySelector('.projects__slider');
+	const projectsSliderCurrent = document.querySelector(
+		'.projects__slider-current',
+	);
 
 	// Create and initialize the animation timeline
 	const timeLine = animateNavLinks();
@@ -20,20 +24,62 @@ document.addEventListener('DOMContentLoaded', () => {
 		timeLine.reversed(!timeLine.reversed());
 	});
 
-	const swiperProjectsImg = new Swiper('.slider-img', {
+	// Initialize Swiper for the projets slider
+	const swiperProjects = new Swiper(projectsSlider, {
 		direction: 'vertical',
 		loop: false,
-		speed: 1800,
+		speed: 1000,
+		// mousewheel: true,
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+			pageUpDown: true,
+		},
+		grabCursor: true,
+		navigation: {
+			nextEl: '.next-slide',
+			prevEl: '.prev-slide',
+			disabledClass: 'slider-btn-disabled',
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true,
+			// horizontalClass: 'swiper-scrollbar-horizontal',
+			// verticalClass: 'swiper-scrollbar-vertical',
+		},
+		pagination: {
+			el: '.projects__slider-total',
+			type: 'custom',
+			renderCustom: function (swiper, current, total) {
+				return `0${total}`;
+			},
+		},
 	});
 
-	const swiperProjectsCards = new Swiper('.slider-cards', {
-		direction: 'vertical',
-		loop: false,
-		speed: 1800,
+	// Update the current slide number when the slide is changes
+	swiperProjects.on('slideChange', () => {
+		const slideIndex = swiperProjects.realIndex;
+		gsap.to(projectsSliderCurrent, 0.2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: () => {
+				gsap.to(projectsSliderCurrent, 0.1, {
+					force3D: true,
+					y: 10,
+				});
+				projectsSliderCurrent.innerHTML = `0${slideIndex + 1}`;
+			},
+		});
+		gsap.to(projectsSliderCurrent, 0.2, {
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			ease: Power2.easeOut,
+			delay: 0.3,
+		});
 	});
-
-	swiperProjectsImg.controller.control = swiperProjectsCards;
-	swiperProjectsCards.controller.control = swiperProjectsImg;
 
 	// Attach a click event handler to the themeBtn to change a theme on button click
 	themeBtn.addEventListener('click', () => {
