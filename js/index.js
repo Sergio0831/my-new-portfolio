d50545438d850cc8ed6dd156a112afef90423c0e
Sergio0831 <sergejs.ivcenko@gmail.com>
2023-10-01 12:06:00 +0100

    Modified projects slider, added direction on desktop to vertical and on smaller screens horizontal

--- js/index.js
+++ js/index.js
@@ -29,49 +29,74 @@ document.addEventListener('DOMContentLoaded', () => {
 		direction: 'vertical',
 		loop: false,
 		speed: 1000,
-		// mousewheel: true,
+		slidesPerView: 1,
+		spaceBetween: 32,
+		grabCursor: true,
+		// Enable keyboard navigation
 		keyboard: {
 			enabled: true,
-			onlyInViewport: false,
+			onlyInViewport: true,
 			pageUpDown: true,
 		},
-		grabCursor: true,
+		// Slider navigation buttons with arrows
 		navigation: {
 			nextEl: '.next-slide',
 			prevEl: '.prev-slide',
 			disabledClass: 'slider-btn-disabled',
 		},
+		// Slider scrollbar
 		scrollbar: {
 			el: '.swiper-scrollbar',
 			draggable: true,
-			// horizontalClass: 'swiper-scrollbar-horizontal',
-			// verticalClass: 'swiper-scrollbar-vertical',
 		},
+		// Custom pagination with numbers
 		pagination: {
 			el: '.projects__slider-total',
 			type: 'custom',
+			// Render total slides
 			renderCustom: function (swiper, current, total) {
 				return `0${total}`;
+			},
+		},
+		breakpoints: {
+			// From 0px and above set slider direction to horizontal and hide scrollbar
+			0: {
+				direction: 'horizontal',
+				scrollbar: {
+					enabled: false,
+				},
+			},
+			// From 1060px and above set slider direction to vertical and show scrollbar
+			1060: {
+				direction: 'vertical',
+				scrollbar: {
+					enabled: true,
+				},
 			},
 		},
 	});
 
 	// Update the current slide number when the slide is changes
 	swiperProjects.on('slideChange', () => {
+		// Find the index of the current slide
 		const slideIndex = swiperProjects.realIndex;
+		// Animate the custom pagination current number to a default state
 		gsap.to(projectsSliderCurrent, 0.2, {
 			force3D: true,
 			y: -10,
 			opacity: 0,
 			ease: Power2.easeOut,
+			// When the animation completes, update the custom pagination current number
 			onComplete: () => {
 				gsap.to(projectsSliderCurrent, 0.1, {
 					force3D: true,
 					y: 10,
 				});
+				// Set the inner HTML of the custom pagination current number to the slide nuimber
 				projectsSliderCurrent.innerHTML = `0${slideIndex + 1}`;
 			},
 		});
+		// Animate the custom pagination current number to its final state
 		gsap.to(projectsSliderCurrent, 0.2, {
 			force3D: true,
 			y: 0,
